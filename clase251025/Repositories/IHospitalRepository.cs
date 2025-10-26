317a12b43ad0650c729b349c6a0c28b00ceab972
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:49:06 -0400

    feat : adding funtion in ihospitalrepository

--- clase251025/Repositories/IHospitalRepository.cs
+++ clase251025/Repositories/IHospitalRepository.cs
@@ -7,5 +7,9 @@ namespace clase251025.Repositories
         Task<IEnumerable<Hospital>> GetAll();
         Task<Hospital> GetOne(Guid id);
         Task Add(Hospital hospital);
+
+        Task Update(Hospital hospital);
+        Task Delete(Guid id);
+        Task<IEnumerable<Hospital>> GetAllByType(int type);
     }
 }
