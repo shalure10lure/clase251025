67a0a41bf2b5c3a587d965ecfd40cee3c72f16a7
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:10:07 -0400

    feat : adding hospitalrepository

---
+++ clase251025/Repositories/HospitalRepository.cs
@@ -0,0 +1,29 @@
+﻿using clase251025.Data;
+using clase251025.Models;
+
+namespace clase251025.Repositories
+{
+    public class HospitalRepository : IHospitalRepository
+    {
+        private readonly AppDbContext _db;
+        public HospitalRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Add(Hospital hospital)
+        {
+            await _db.Hospitals.AddAsync(hospital);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Hospital>> GetAll()
+        {
+            return await _db.Hospitals.ToListAsync();
+        }
+
+        public async Task<Hospital> GetOne(Guid id)
+        {
+            return await _db.Hospitals.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
