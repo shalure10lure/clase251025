6418efe823548f5aa5ae7c0e627c788a335df5b8
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:34:11 -0400

    feat : adding dto register

---
+++ clase251025/Models/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace clase251025.Models.DTOs
+{
+    public record RegisterDto
+    {
+        public string Username { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        public string Role { get; set; } = "User";
+    }
+}
