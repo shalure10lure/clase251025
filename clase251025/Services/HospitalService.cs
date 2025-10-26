add67ce540c3baf6dca9cc8b16cff675e8d4cb3a
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:06:16 -0400

    feat : adding hospitalservice

---
+++ clase251025/Services/HospitalService.cs
@@ -0,0 +1,36 @@
+﻿using clase251025.Models;
+using clase251025.Models.DTOs;
+
+namespace clase251025.Services
+{
+    public class HospitalService : IHospitalService
+    {
+        private readonly IHospitalRepository _repo;
+        public HospitalService(IHospitalService repo)
+        {
+            _repo = repo;
+        }
+        public Task<Hospital> CreateHospital(CreateHospitalDto dto)
+        {
+            var hospital = new Hospital
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Address = dto.Address,
+                Type = dto.Type
+            };
+            await _repo.Add(hospital);
+            return hospital;
+        }
+
+        public async Task<IEnumerable<Hospital>> GetAll()
+        {
+            return await _repo.GetAll();
+        }
+
+        public Task<Hospital> GetOne(Guid id)
+        {
+            return _repo.GetOne(id);
+        }
+    }
+}
