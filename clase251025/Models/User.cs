7e45ebee843895b44c04c00d5d57189a4a998745
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:31:22 -0400

    feat : adding models user

---
+++ clase251025/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace clase251025.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin"
+    }
+}
