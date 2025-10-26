b940d53ba8bc215407e993beb180d4038df397cf
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:02:27 -0400

    feat : adding hospitalcontroller

---
+++ clase251025/Controllers/HospitalController.cs
@@ -0,0 +1,40 @@
+﻿using clase251025.Models;
+using clase251025.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clase251025.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HospitalController:ControllerBase
+    {
+        private readonly IHospitalService _service;
+        public HospitalController(IHospitalService service )
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllHospitals()
+        {
+            IEnumerable<Hospital> items = await _service.GetAll();
+            return Ok(items);
+        }
+        [HttpGet("{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> GetOne(Guid id)
+        {
+            var hospital = await _service.GetOne(id);
+            return Ok(hospital);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> CreateHospital([FromBody] CreateHospitalDto dto)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var hospital = await _service.CreateHospital(dto);
+            return CreatedAtAction(nameof(GetOne), new { id = hospital.Id }, hospital);
+        }
+    }
+}
