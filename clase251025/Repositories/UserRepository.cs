26a09c709324a62d2fcc0450a582a1a52f28257c
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:56:11 -0400

    feat : adding UserRepository

---
+++ clase251025/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using clase251025.Models;
+
+namespace clase251025.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async  Task<User> GetByEmailAddress(string emailAddress)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == emailAddress);
+        }
+    }
+}
