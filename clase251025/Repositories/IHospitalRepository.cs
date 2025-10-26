0cdb2b72823b3a28d8cdd033e780d548fd1984b3
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:07:09 -0400

    feat : adding Ihospitalrepository

---
+++ clase251025/Repositories/IHospitalRepository.cs
@@ -0,0 +1,11 @@
+﻿using clase251025.Models;
+
+namespace clase251025.Repositories
+{
+    public interface IHospitalRepository
+    {
+        Task<IEnumerable<Hospital>> GetAll();
+        Task<Hospital> GetOne(Guid id);
+        Task Add(Hospital hospital);
+    }
+}
