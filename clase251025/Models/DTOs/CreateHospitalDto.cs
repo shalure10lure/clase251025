fba3133c3b801f3721da425cc81b8b2a3f2611b9
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:35:39 -0400

    feat : adding dto createhospital

---
+++ clase251025/Models/DTOs/CreateHospitalDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clase251025.Models.DTOs
+{
+    public class CreateHospitalDto
+    {
+        [Required]
+        public Guid Id { get; init; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Type { get; set; }
+    }
+}
