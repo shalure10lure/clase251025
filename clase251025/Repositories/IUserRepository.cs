79c6ab96c098054c07c8974594b2f82c2d9caa04
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:53:01 -0400

    feat : adding IUserRepository

---
+++ clase251025/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using clase251025.Models;
+
+namespace clase251025.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmailAddress(string emailAddress);
+        Task AddAsync(User user);
+    }
+}
