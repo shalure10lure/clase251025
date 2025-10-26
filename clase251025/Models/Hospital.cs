cf1c028225e8de99501cad6bfeee5cdee556a9a5
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:34:49 -0400

    feat : adding model hospital

---
+++ clase251025/Models/Hospital.cs
@@ -0,0 +1,10 @@
+﻿namespace clase251025.Models
+{
+    public class Hospital
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Type { get; set; }
+    }
+}
