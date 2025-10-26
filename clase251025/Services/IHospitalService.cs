b0d373bf1fe05b4bf2279b9264e5baf5d5b0f9ee
shalure10lure <yessicarado@gmail.com>
2025-10-26 16:03:21 -0400

    feat : adding Ihospitalservice

---
+++ clase251025/Services/IHospitalService.cs
@@ -0,0 +1,12 @@
+﻿using clase251025.Models;
+using clase251025.Models.DTOs;
+
+namespace clase251025.Services
+{
+    public interface IHospitalService
+    {
+        Task<IEnumerable<Hospital>> GetAll();
+        Task<Hospital> GetOne(Guid id);
+        Task<Hospital> CreateHospital(CreateHospitalDto dto);
+    }
+}
