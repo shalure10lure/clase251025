25fb053ea0ef9a85481d30a365f6a6117f7a8d31
shalure10lure <yessicarado@gmail.com>
2025-10-26 17:02:02 -0400

    feat : adding dto updatehospital

---
+++ clase251025/Models/DTOs/UpdateHospitalDto.cs
@@ -0,0 +1,9 @@
+﻿namespace clase251025.Models.DTOs
+{
+    public record UpdateHospitalDto
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int? Type { get; set; } 
+    }
+}
