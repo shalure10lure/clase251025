4d931c5838e4648c03d2c1c8574db9795d426a76
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:58:58 -0400

    feat : adding Appdbcontext

---
+++ clase251025/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using clase251025.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace clase251025.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>();
+            modelBuilder.Entity<Hospital>();
+        }
+    }
+}
