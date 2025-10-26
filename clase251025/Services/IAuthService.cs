7264a298f016d25d766d1dae203235abcaaa8749
shalure10lure <yessicarado@gmail.com>
2025-10-26 15:47:06 -0400

    feat : adding IAuthservice

---
+++ clase251025/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using clase251025.Models.DTOs;
+
+namespace clase251025.Services
+{
+    public interface IAuthService
+    {
+        Task<string> RegisterAsync(RegisterDto dto);
+        Task<bool> VerifyCredentials(LoginDto dto);
+        Task<bool ok, string? token> LoginAsync(LoginDto dto);
+    }
+}
